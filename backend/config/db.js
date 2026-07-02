92ecf0f06f21f0cd419bf2aa82950cd2eb64f902
prajina-Adhikari <prajinaadhkari5@gmail.com>
2026-07-02 20:43:39 +0545

    new changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connectDB() {
   try {
+     console.log("MONGO_URI:", process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
