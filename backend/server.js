314a6e72a4edc4167454a3701f1352f4a1e55d76
prajina-Adhikari <prajinaadhkari5@gmail.com>
2026-07-04 15:46:18 +0545

    new changes

--- backend/server.js
+++ backend/server.js
@@ -30,4 +30,11 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Mock Mitra API running on port ${PORT}`));
+
+if (process.env.NODE_ENV === "development") {
+  app.listen(PORT, () => {
+    console.log(`Mock Mitra API running on port ${PORT}`);
+  });
+}
+
+export default app;
